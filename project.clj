ec262aa3730d8cef1a3ee2e29be449a06cdaf7eb
Sumit Mahamuni <sumitmah@thoughtworks.com>
2014-04-27 15:24:13 +0530

    Sumit: Fixing project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject practice-exacmples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure examples"
+  :url "http://github.com/sumitmah"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
